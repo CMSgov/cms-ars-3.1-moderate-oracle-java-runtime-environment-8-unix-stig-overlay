da954cd9824509ce6480609a97a78b2e01121c9b
Haynes Jr <dhaynes@mitre.org>
2019-03-29 10:39:01 -0400

    adding draft overlay and updated spreadsheet.

---
+++ controls/overlay.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+include_controls 'nginx-baseline' do
+  control 'V-66919' do
+
+  end
+
+  control 'V-66923' do
+
+  end
+
+  control 'V-66933' do
+
+  end
+
+  control 'V-66935' do
+
+  end
+end
