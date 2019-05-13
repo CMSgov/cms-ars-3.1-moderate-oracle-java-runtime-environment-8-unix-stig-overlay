20205e52b5ab4017060ddc0a107fcf22b5070b24
Haynes Jr <dhaynes@mitre.org>
2019-05-13 10:18:44 -0400

    fixed a couple bugs and updated based on EA feedback.

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,19 +1,23 @@
 # encoding: utf-8
-
-include_controls 'oracle-java-runtime-environment-7-unix-stig-baseline' do
+                 
+include_controls 'oracle-java-runtime-environment-8-unix-stig-baseline' do
   control 'V-66919' do
-
+    tag "cci": ['CCI-001162']
+    tag "nist": ['SC-18', 'Rev_4']
   end
 
   control 'V-66923' do
-
+    tag "cci": ['CCI-001162']
+    tag "nist": ['SC-18', 'Rev_4']
   end
 
   control 'V-66933' do
-
+    tag "cci": ['CCI-001162']
+    tag "nist": ['SC-18', 'Rev_4']
   end
 
   control 'V-66935' do
-
+    tag "cci": ['CCI-001228']
+    tag "nist": ['SI-2', 'Rev_4']
   end
 end
