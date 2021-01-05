ba9f3e6f1807a495687b2a49ee58542a9e06aaf8
karikarshivani <karikarshivani@gmail.com>
2021-01-05 11:51:21 -0500

    Replaced modified NIST tags with appropriate caveats
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,23 +1,24 @@
 # encoding: utf-8
                  
 include_controls 'oracle-java-runtime-environment-8-unix-stig-baseline' do
+
   control 'V-66919' do
-    tag "cci": ['CCI-001162']
-    tag "nist": ['SC-18', 'Rev_4']
+    impact 0.0
+		desc 'caveat', 'Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1'
   end
 
   control 'V-66923' do
-    tag "cci": ['CCI-001162']
-    tag "nist": ['SC-18', 'Rev_4']
+    timpact 0.0
+		desc 'caveat', 'Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1'
   end
 
   control 'V-66933' do
-    tag "cci": ['CCI-001162']
-    tag "nist": ['SC-18', 'Rev_4']
+    impact 0.0
+		desc 'caveat', 'Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1'
   end
 
   control 'V-66935' do
-    tag "cci": ['CCI-001228']
-    tag "nist": ['SI-2', 'Rev_4']
+    impact 0.0
+		desc 'caveat', 'Not applicable for this CMS ARS 3.1 overlay, since the related security control is not included in CMS ARS 3.1'
   end
 end
